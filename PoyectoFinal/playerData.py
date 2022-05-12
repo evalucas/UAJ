bab12df87ecfce61c42f4495b9571f182359cc0e
jeramauni <jeramauni@gmail.com>
2022-05-12 19:19:03 +0200

    Despausa

--- PoyectoFinal/playerData.py
+++ PoyectoFinal/playerData.py
@@ -10,7 +10,9 @@ class Player:
     nDeaths = None #muertes por cada nivel
     paths = None #caminos recorridos por cada nivel
     nPause = 0 #numero de veces que ha pausado
-
+    levelsCompleted = -1    #niveles completados por el jugador
+    tiempoEnPausa = 0       #Tiempo que el jugador se pasa en el menu de pausa
+    pauseTime = 0   
     #DATOS DEL FORMULARIO
     age = None
     generalExp = -1
@@ -45,8 +47,16 @@ class Player:
     def death(self,actualLevel):
         self.nDeaths[actualLevel]+=1
         self.resetPath(actualLevel) #borra el camino guardado hasta el momento, para quedarse con el último de cada nivel
-    def pause(self):
-        self.nPause+=1
+
+    def pause(self,tiempo):
+        if(self.pauseTime != 0):                            #si hay un primer pause                      
+            self.tiempoEnPausa += (tiempo - self.pauseTime) #Anade al tiempo de pausa total el de esta pausa
+            self.pauseTime = 0 
+        else:                                               #si no, cuenta una pausa
+            self.pauseTime += tiempo                        #guarda el timestamp
+            self.nPause += 1
+
+
     def addToPath(self,pos,actualLevel):
         #añadir una posicion a un camino
         self.paths[actualLevel].append(pos)
@@ -55,13 +65,20 @@ class Player:
         self.paths[actualLevel].clear()
         #print("CLEARING PATH ", actualLevel, "...")
         #print(self.paths[actualLevel])
+
     def end(self):
         #utilizo el sessionEnd para hacer el print final
         self.informe()
+
+    #Nivel finalizado +1 al contador de niveles superados
+    def lvlEnd(self):
+        self.levelsCompleted += 1
+
     def informe(self): #comento todo el metodo para que no se pete la consola de datos
         print("INFORME JUGADOR ", self.ID)
         print("Jumps: ", self.nJumps)
         print("Deaths: ", self.nDeaths)
+        print("Tiempo en pausa: ", self.tiempoEnPausa)
         for i in range(len(self.paths)):
             print("Path ", i, ": ", self.paths[i])
         print("---------------------------------------------")
